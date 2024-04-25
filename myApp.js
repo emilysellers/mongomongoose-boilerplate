eb8eae1ec7b89f4e96f7053745118c76025553a7
Emily Sellers <emilymelindasellers@gmail.com>
2024-04-25 12:09:19 -0700

    Connect to database

--- myApp.js
+++ myApp.js
@@ -1,5 +1,14 @@
-require('dotenv').config();
+require("dotenv").config();
+const mongoose = require("mongoose");
 
+// connect to database
+mongoose.connect(
+  process.env.MONGO_URI
+  //, {
+  //useNewUrlParser: true,
+  //useUnifiedTopology: true,
+  //}
+);
 
 let Person;
 
